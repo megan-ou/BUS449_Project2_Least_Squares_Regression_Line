01b46cc46587ea37c4cde0f1f3f95f585a38b0b1
meganou <ou27@up.edu>
2025-09-21 17:05:34 -0700

    debug lin3 73 in regress.py

--- regress.py
+++ regress.py
@@ -70,7 +70,9 @@ def regress_comp(y, x):
     if len(x) != len(y):
         return -1
 
-    if all([isinstance(x[i], Number) or isinstance(y[i], Number) for i in range(len(x))]):
+    if all([isinstance(x[i], Number) for i in range(len(x))]):
+        return -4
+    if all([isinstance(y[i],Number) for i in range(len(y))]):
         return -4
 
     beta_0 = 0
