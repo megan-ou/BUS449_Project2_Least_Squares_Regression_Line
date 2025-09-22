c4c3772f790bbe9e65290a7f64d39c66fe098afb
meganou <ou27@up.edu>
2025-09-22 10:56:50 -0700

    Add comments and headers to regress.py

--- regress.py
+++ regress.py
@@ -1,6 +1,6 @@
 #regress.py
-#Calculates Least Squares Regression Line for a set of data
-# Finds beta_0 and beta_1 coefficients
+#Calculates Least Squares Regression Line for a set of data in two different ways.
+#Finds beta_0 and beta_1 coefficients
 #@Author Megan Ou, Toby Okoji
 #@Version September 2025
 
